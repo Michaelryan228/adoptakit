384b8f25b2518648b5fc91c4d18ad4f528689835
Michael Ryan <michaelryan2895@gmail.com>
2021-02-15 15:13:15 -0500

    all base

---
+++ app.js
@@ -0,0 +1,18 @@
+let kittenContainerElem = document.getElementById('kittenProfiles');
+
+let articleElem = document.createElement('article');
+
+kittenContainerElem.appendChild(articleElem);
+
+let nameHeaderElem = document.createElement('h2');
+
+nameHeaderElem.textContent = 'Frankie';
+
+articleElem.appendChild(nameHeaderElem);
+
+/*
+<article>
+  <h2>.....</h2>
+</article>
+console.log(kittenContainerElem);
+*/
